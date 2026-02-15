807b06f8227a84a59e1b7a8828fd395040028fb9
Vatsal <vjg239@gmail.com>
2026-02-15 08:47:38 +0530

    feat: Change it to a new model for simply storing encrypted json

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -18,8 +18,8 @@ load_dotenv(os.path.join(root_path, '.env'))
 from models import *
 
 # 3. Read the URL
-db_url = os.getenv("SUPABASE_URL")
-# print(f"Database url formigrations: {db_url}")
+db_url = os.getenv("DATABASE_URL")
+# print(f"Database url for migrations: {db_url}")
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
