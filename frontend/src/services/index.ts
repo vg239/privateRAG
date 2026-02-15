1ddd0c6c6abef5c604cdb15d715e549a093e041a
Vatsal <vjg239@gmail.com>
2026-02-15 09:14:17 +0530

    feat : write services for backend connection to the vaults router

---
+++ frontend/src/services/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Services - API clients for backend communication
+ */
+
+export {
+  createVault,
+  listVaults,
+  listVaultSummaries,
+  getVault,
+  deleteVault,
+  type VaultData,
+  type VaultResponse,
+  type VaultSummary,
+  type VaultListResponse,
+} from "./vault";
+
