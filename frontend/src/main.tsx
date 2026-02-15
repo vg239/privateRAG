f3474b46df7e8a57f7f9fa1b0ff5afb6a854eb1f
Vatsal <vjg239@gmail.com>
2026-02-15 09:16:22 +0530

    update old frontend implementations and add the client.ts for interacting with backend router with the service

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
+import { Landing, AppPage, Docs } from './pages'
+import { Chats } from './pages/Chats'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/app" element={<AppPage />} />
+      <Route path="/docs" element={<Docs />} />
+      <Route path="/chats" element={<Chats />} />
+    </Routes>
+  </BrowserRouter>
 )
